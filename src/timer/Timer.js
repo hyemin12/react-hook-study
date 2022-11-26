7cb530b60a40a7288e0993999d7a6162299bb058
hyemin12 <khnm31@naver.com>
2022-11-26 18:26:54 +0900

    '시간입력하는타이머만들기'

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -5,18 +5,69 @@ import { useState } from "react";
 function Timer() {
   const countRef = useRef();
   const [count, setCount] = useState(0);
+  const [paused, setPaused] = useState(true);
   const handleCountStart = () => {
-    countRef.current = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
+    setPaused(false);
+    if (!paused) {
+      countRef.current = setInterval(() => {
+        setCount((count) => count + 1);
+      }, 1000);
+    }
   };
+  // useEffect(() => {
+  //   handleCountStart();
+  // }, []);
   const handleCountStop = () => {
     clearInterval(countRef.current);
-    countRef.current = null;
+    setPaused(true);
   };
-  useEffect(() => {
-    handleCountStart();
-  }, []);
+
+  /** 시간 타이머 */
+  const timerRef = useRef(null);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  let totalTime = parseInt(minutes * 60 + seconds);
+
+  console.log(typeof totalTime, totalTime / 60);
+
+  const [countMin, setCountMin] = useState(Math.floor(totalTime / 60));
+  const [countSec, setCountSec] = useState(Math.floor(totalTime % 60));
+
+  const onChange = (e) => {
+    const { value, id } = e.target;
+    if (id === "minutes") {
+      setMinutes(value);
+    } else if (id === "seconds") {
+      if (parseInt(value) > 59) {
+        // const min = parseInt(value / 60);
+        // console.log(value, min);
+        // setMinutes(minutes + min);
+      }
+      setSeconds(value);
+      setCountSec(value);
+    }
+  };
+  const convertFormat = (num) => {
+    String(num).padStart(2, "0");
+  };
+  const countStart = () => {
+    if (timerRef.current !== null) {
+      return;
+    }
+    timerRef.current = setInterval(() => {
+      setMinutes(convertFormat(parseInt(totalTime / 60)));
+      setSeconds(convertFormat(parseInt(totalTime % 60)));
+    }, 1000);
+  };
+  const countStop = () => {
+    if (timerRef.current === null) {
+      return;
+    }
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  };
+  // console.log(minutes, seconds, countSec);
   return (
     <>
       <div>
@@ -24,7 +75,18 @@ function Timer() {
         <button onClick={handleCountStart}>시작</button>
         <button onClick={handleCountStop}>멈춤</button>
       </div>
-      <div></div>
+      <div style={{ padding: "50px 0" }}>
+        <label>분 : </label>
+        <input id="minutes" type="number" value={minutes} onChange={onChange} />
+        <label>초 : </label>
+        <input id="seconds" type="number" value={seconds} onChange={onChange} />
+        <h4>
+          {String(countMin).padStart(2, "0")}:
+          {String(countSec).padStart(2, "0")}
+        </h4>
+        <button onClick={countStart}>시작</button>
+        <button onClick={countStop}>멈춤</button>
+      </div>
     </>
   );
 }
