fa302ac86419f3937e2bc221a5f249237fa89e9c
hyemin12 <khnm31@naver.com>
2022-11-28 17:59:50 +0900

    fullScreen

--- src/useFullScreen/UseFullScreen.js
+++ src/useFullScreen/UseFullScreen.js
@@ -1,5 +1,41 @@
+import { useRef } from "react";
+
+const useFullScreen = () => {
+  const element = useRef();
+  const triggerFull = () => {
+    if (element.current.requestFullscreen)
+      return element.current.requestFullscreen();
+    if (element.current.webkitRequestFullscreen)
+      return element.current.webkitRequestFullscreen();
+    if (element.current.mozRequestFullScreen)
+      return element.current.mozRequestFullScreen();
+    if (element.current.msRequestFullscreen)
+      return element.current.msRequestFullscreen();
+  };
+  const exitFull = () => {
+    if (document.exitFullscreen) return document.exitFullscreen();
+    if (document.webkitCancelFullscreen)
+      return document.webkitCancelFullscreen();
+    if (document.mozCancelFullScreen) return document.mozCancelFullScreen();
+    if (document.msExitFullscreen) return document.msExitFullscreen();
+  };
+  return { element, triggerFull, exitFull };
+};
+
 const AppUseFullScreen = () => {
-  return <div></div>;
+  const { element, triggerFull, exitFull } = useFullScreen();
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1544212415-b269f18bf7ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+        alt="이미지!"
+        ref={element}
+      />
+
+      <button onClick={triggerFull}>Make FullScreen</button>
+      <button onClick={exitFull}>Exit FullScreen</button>
+    </div>
+  );
 };
 
 export default AppUseFullScreen;
